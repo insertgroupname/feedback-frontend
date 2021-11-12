6df371d211ef3b7733f7ee8d34033a27a2004ad5
flukerbooker <win.vorapol52@gmail.com>
2021-11-12 13:58:41 +0700

    add filter rendering

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,16 @@
+import { useState } from 'react';
 import { Helmet } from 'react-helmet';
-import { Box, Container, Grid, CircularProgress } from '@material-ui/core';
+import {
+  Box,
+  Container,
+  Grid,
+  Card,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem
+} from '@material-ui/core';
 import LandingToolbar from '../components/landing/LandingToolbar';
 import LandingCard from 'src/components/landing/LandingCard';
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,6 +23,12 @@ import { openEditModal } from 'src/redux/actions/modalActions';
 const Landing = () => {
   const dispatch = useDispatch();
 
+  const [tag, setTag] = useState('All');
+
+  const handleChange = (event) => {
+    setTag(event.target.value);
+  };
+
   const openEditModalHandler = (videoUUID) => {
     dispatch(openEditModal(videoUUID));
   };
@@ -20,7 +37,26 @@ const Landing = () => {
   const { isLoading: isItemLoading, items, error } = itemsState;
 
   const settingsState = useSelector((state) => state.settings);
-  const { isLoading: isSettingLoading, username } = settingsState;
+  const { isLoading: isSettingLoading, username, tags } = settingsState;
+
+  const selectedTag = items.map((item) => {
+    return {
+      ...item,
+      selectedTag: item.tags.find((value) => {
+        return value === tag;
+      })
+    };
+  });
+
+  let formatItem;
+
+  if (tag !== 'All') {
+    formatItem = selectedTag.filter((ele) => {
+      return ele.selectedTag !== undefined;
+    });
+  } else {
+    formatItem = items;
+  }
 
   return (
     <>
@@ -54,10 +90,35 @@ const Landing = () => {
             }}
           >
             <LandingToolbar username={username} itemLength={items.length} />
+            <Card
+              sx={{
+                mt: 3,
+                width: 'fit-content'
+              }}
+            >
+              <FormControl sx={{ m: 1, minWidth: 80 }}>
+                <InputLabel id="filterTag">Filter Tag</InputLabel>
+                <Select
+                  labelId="filterTag"
+                  id="filterTag"
+                  value={tag}
+                  onChange={handleChange}
+                  autoWidth
+                  label="Tag"
+                >
+                  <MenuItem value="All">All</MenuItem>
+                  {tags.map((ele, index) => (
+                    <MenuItem key={index} value={ele}>
+                      {ele}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Card>
             <Box sx={{ pt: 3 }}>
               <Grid container spacing={3}>
                 {items.length > 0 ? (
-                  items.map((item) => (
+                  formatItem.map((item) => (
                     <Grid item key={item.videoUUID} lg={4} md={6} xs={12}>
                       <LandingCard
                         item={item}
